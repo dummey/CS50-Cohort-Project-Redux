1791e74274f6d74750f70645a5ebd7e51a30c596
Ricky Ng <dummey@gmail.com>
2015-06-24 18:52:44 -0500

    Hey a background.

--- damnit.rb
+++ damnit.rb
@@ -2,8 +2,18 @@ require 'gosu'
 
 class MyWindow < Gosu::Window
   def initialize
-   super(640, 480, false)
-   self.caption = 'Damnit!'
+    super(640, 480, false)
+    self.caption = 'Damnit!'
+
+    @background_image = Gosu::Image.new("media/Backgrounds/purple.png", :tileable => true)
+  end
+
+  def update
+
+  end
+
+  def draw
+    @background_image.draw(100, 100, 0)
   end
 end
 
