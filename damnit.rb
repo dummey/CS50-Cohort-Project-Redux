bd9f0038f8b59c1ce13296cdcb05b2ee18a1f838
Ricky Ng <dummey@gmail.com>
2015-07-01 18:24:08 -0500

    Extracted background into its own class

--- damnit.rb
+++ damnit.rb
@@ -5,8 +5,7 @@ class MyWindow < Gosu::Window
     super(800, 600, false)
     self.caption = 'Damnit!'
 
-    @background_image = Gosu::Image.new("media/Backgrounds/purple.png", :tileable => true)
-    @z = 0
+    @background = Background.new(self.width, self.height)
   end
 
   def update
@@ -14,15 +13,37 @@ class MyWindow < Gosu::Window
   end
 
   def draw
-    x = (self.width/@background_image.width).to_i
-    y = (self.height/@background_image.height).to_i
-    for i in -1..x
-      for j in -1..y
-        @background_image.draw(i*@background_image.width + @z % @background_image.width, j*@background_image.height + @z % @background_image.height, 0)
+    @background.draw
+  end
+end
+
+class Background
+  def initialize(width, height)
+    @background_image = Gosu::Image.new("media/Backgrounds/purple.png", :tileable => true)
+  
+    @x_num = (width/@background_image.width).to_i
+    @y_num = (height/@background_image.height).to_i
+
+    @count = 0
+  end
+
+  def draw 
+    for i in -1..@x_num
+      for j in -1..@y_num
+        @background_image.draw(i*@background_image.width + @count % @background_image.width, j*@background_image.height + @count % @background_image.height, 0)
       end
     end
-    @z-=1
+
+    @count -= 1
   end
+end
+
+class Player
+
+end
+
+class Astroids
+
 end
 
 window = MyWindow.new
