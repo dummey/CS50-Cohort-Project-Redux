7ec27f9048022164910791997df7106b4e8d0fb0
Ricky Ng <dummey@gmail.com>
2015-07-01 18:33:18 -0500

    Moving update logic of background.

--- damnit.rb
+++ damnit.rb
@@ -9,7 +9,7 @@ class MyWindow < Gosu::Window
   end
 
   def update
-
+    @background.update
   end
 
   def draw
@@ -24,26 +24,40 @@ class Background
     @x_num = (width/@background_image.width).to_i
     @y_num = (height/@background_image.height).to_i
 
-    @count = 0
+    @cycle = 0
+  end
+
+  def update
+    @cycle -= 1
   end
 
   def draw 
     for i in -1..@x_num
       for j in -1..@y_num
-        @background_image.draw(i*@background_image.width + @count % @background_image.width, j*@background_image.height + @count % @background_image.height, 0)
+        @background_image.draw(i*@background_image.width + @cycle % @background_image.width, j*@background_image.height + @cycle % @background_image.height, 0)
       end
     end
-
-    @count -= 1
   end
 end
 
 class Player
+  def initialize
 
+  end
+
+  def update 
+
+  end
+
+  def draw
+
+  end
 end
 
 class Astroids
+  def initialize
 
+  end
 end
 
 window = MyWindow.new
