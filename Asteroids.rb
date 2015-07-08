d784c4a8fcc752532ae2b3366a018be1a9e358ae
Ricky Ng <dummey@gmail.com>
2015-07-08 14:51:26 -0500

    Added background music and abstracting media path.

--- Asteroids.rb
+++ Asteroids.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
+$MEDIA_ROOT = File.dirname(__FILE__) + '/media'
 
 require 'gosu'
 require 'scenes/game_scene'
@@ -10,9 +11,11 @@ class MyWindow < Gosu::Window
 
 
     @background = Background.new(self.width, self.height)
+    @bg_music = Gosu::Song.new($MEDIA_ROOT + "/Music/80s-Space-Game-Loop_v001.mp3")
   end
 
   def update
+    @bg_music.play(true) unless @bg_music.playing?
     @background.update
   end
 
@@ -23,7 +26,7 @@ end
 
 class Background
   def initialize(width, height)
-    @background_image = Gosu::Image.new("media/Backgrounds/purple.png", :tileable => true)
+    @background_image = Gosu::Image.new($MEDIA_ROOT + "/Backgrounds/purple.png", :tileable => true)
 
     @x_num = (width/@background_image.width).to_i
     @y_num = (height/@background_image.height).to_i
