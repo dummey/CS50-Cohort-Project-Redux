d482d0f5c08e7df82b02e6f6fa14a988ecc23b89
Carol <museum.mile@icloud.com>
2015-07-08 19:58:02 -0500

    updates for modulo

--- lib/game_objects/asteroid.rb
+++ lib/game_objects/asteroid.rb
@@ -1,15 +1,19 @@
 class Asteroid
   def initialize(width, height)
+    @window_width=width
+    @window_height=height
     @asteroid_image = Gosu::Image.new("media/PNG/Meteors/meteorGrey_big1.png", :tileable => true)
-    @x_velocity = rand(0...4)
-    @y_velocity = rand(0...4)
-    @x_position = 100
-    @y_position = 100
+    @x_velocity = rand(-4...4)
+    @y_velocity = rand(-4...4)
+    @x_position = rand(0...width)
+    @y_position = rand(0...height)
   end
 
   def update
-@x_position = @x_position + @x_velocity
-@y_position = @y_position + @y_velocity
+    @x_position = @x_position + @x_velocity
+    @y_position = @y_position + @y_velocity
+    @x_position=@x_position.modulo(@window_width)
+    @y_position=@y_position.modulo(@window_height)
   end
 
   def draw
