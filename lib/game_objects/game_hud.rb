ff5a6c7f47911a0f9d4cfff1912255e8988cc6fb
Ricky Ng <dummey@gmail.com>
2015-07-16 12:52:16 -0500

    Tied game_huds lives and score to the game scene.

--- lib/game_objects/game_hud.rb
+++ lib/game_objects/game_hud.rb
@@ -5,23 +5,23 @@ class GameHUD
     @lives = 3
     @lives_icon = Gosu::Image.new($MEDIA_ROOT + "/PNG/UI/playerLife3_green.png")
     @score = 0
-    @font = Gosu::Font.new(25)
+    @score_font = Gosu::Font.new(25)
 
   end
 
   def _draw_lives
-    @lives.times do |i|
+    @scene.lives.times do |i|
       @lives_icon.draw(10 + 40 * i, 10, 1)
     end
   end
 
   def _draw_score
-
+    @scene.score
+    @score_font.draw("Score: #{@scene.score}", 10,40,1)
   end
 
   def draw
-    # @font.draw("Lives: #{@lives}", 10,10,1)
     self._draw_lives
-    @font.draw("Score: #{@score}", 10,40,1)
+    self._draw_score
   end
 end
