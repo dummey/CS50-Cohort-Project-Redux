76083ad3f980830499c0e4785a9d71310b0abe49
Ricky Ng <dummey@gmail.com>
2015-07-19 20:50:43 -0500

    adding convience methods to game object for centered drawing.

--- lib/game_object.rb
+++ lib/game_object.rb
@@ -10,4 +10,16 @@ class GameObject
   def demolish? 
     @demolish
   end
+
+  def _centered
+    return [0.5, 0.5, @scale, @scale]
+  end
+
+  def _image_pos_centered(x_off = 0, y_off = 0)
+    return [@x_pos + x_off, @y_pos + y_off, @z_index, @angle, *self._centered]
+  end
+
+  def _text_pos_centered(x_off = 0, y_off = 0)
+    return [@x_pos + x_off, @y_pos + y_off, @z_index, *self._centered]
+  end
 end
