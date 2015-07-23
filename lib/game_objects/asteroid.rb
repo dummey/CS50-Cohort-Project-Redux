146c461ac1f9a8525a565be2fe4ab965f4b62347
Carol <museum.mile@icloud.com>
2015-07-22 19:54:50 -0500

    update for more asteroidy speed and rotation

--- lib/game_objects/asteroid.rb
+++ lib/game_objects/asteroid.rb
@@ -5,24 +5,28 @@ class Asteroid < GameObject
     super(scene)
 
     @asteroid_image = Gosu::Image.new("media/PNG/Meteors/meteorGrey_big1.png", :tileable => true)
-    @x_velocity = rand(-4...4)
-    @y_velocity = rand(-4...4)
+    @x_velocity = rand(-100...100)
+    @y_velocity = rand(-100...100)
     @x_position = rand(0...@scene.width)
     @y_position = rand(0...@scene.height)
-    @rotation_momentum = 5
-    @rotation_angular = 0
+    @rotation_momentum = rand(-10...10)
+    @rotation_angular = rand(-10...10)
   end
 
   def update
-    @x_position = @x_position + @x_velocity
-    @y_position = @y_position + @y_velocity
+    update_in_seconds = @scene.update_interval / 1000.0
+    @x_position = @x_position + @x_velocity * update_in_seconds
+    @y_position = @y_position + @y_velocity * update_in_seconds
     @x_position=@x_position.modulo(@scene.width)
     @y_position=@y_position.modulo(@scene.height)
-    @rotation_angular = (@rotation_angular + @rotation_momentum)
+    @rotation_angular = (@rotation_angular + @rotation_momentum * @scene.update_interval / 1000.0)
   end
 
   def draw
 #    @asteroid_image.draw(@x_position, @y_position, 1)
     @asteroid_image.draw_rot(@x_position, @y_position, 1, @rotation_angular)
+  end
+
+  def die
   end
 end
