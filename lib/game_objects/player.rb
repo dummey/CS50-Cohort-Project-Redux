16148b2899c627fbc0c1c3d27240866b69bbd3fd
Ther <emeraldvision@users.noreply.github.com>
2015-08-19 19:11:26 -0500

    refactor to use draw helper in player

--- lib/game_objects/player.rb
+++ lib/game_objects/player.rb
@@ -1,22 +1,30 @@
 require 'game_object'
-require 'pp'
+require 'game_objects/role/draw_helper'
 
 class Player < GameObject
+  include DrawHelper
+  
   def initialize(scene)
     super(scene)
-    @image = Gosu::Image.new("media/PNG/playerShip3_green.png")
+    @image_path = "media/PNG/playerShip3_green.png"
     @body = CP::Body.new(10.0, 150.0)
-    @shape_boundary = CP::Shape::Circle.new(@body, @image.width/2, CP::Vec2::ZERO)
+    @z_index = 1
+    @scale = 1
+    @shape_boundary = CP::Shape::Circle.new(@body, self.image.width/2, CP::Vec2::ZERO)
     @shape_boundary.sensor = true
     @shape_boundary.collision_type = :player_sensor
-    @shape_collide = CP::Shape::Circle.new(@body, @image.width/4, CP::Vec2::ZERO)
+    @shape_collide = CP::Shape::Circle.new(@body, self.image.width/4, CP::Vec2::ZERO)
     @shape_collide.collision_type = :player
     @body.p = CP::Vec2.new(@scene.width/2, @scene.height/2)
     @body.a = 0.gosu_to_radians
     scene.space.add_body(@body)
     scene.space.add_shape(@shape_boundary)
     scene.space.add_shape(@shape_collide)
-    @boundary = { left_edge: false, right_edge: false, top_edge: false, bottom_edge: false}
+    self.setup_boundary
+  end
+
+  def body
+    return @body
   end
 
   def display_ghost(edge, enabled)
@@ -35,53 +43,19 @@ class Player < GameObject
     #wrap around the field
     @body.p.x = @body.p.x % @scene.width
     @body.p.y = @body.p.y % @scene.height
-
-    if @reset
-      @body.activate
-      @body.p = CP::Vec2.new(@scene.width/2, @scene.height/2)
-      @body.a = 0.gosu_to_radians
-      @body.v = CP::Vec2::ZERO
-      @body.w = 0
-      @body.reset_forces
-
-      @reset = false
-    end 
   end
 
   def reset
-    @reset = true
+    @body.p = CP::Vec2.new(@scene.width/2, @scene.height/2)
+    @body.a = 0.gosu_to_radians
+    @body.v = CP::Vec2::ZERO
+    @body.w = 0
+    @body.reset_forces
+    #add invulnerability
   end
 
   def draw
-    # (x, y, z, angle, center_x, center_y, scale_x, scale_y)
-    # draw main
-    @image.draw_rot(@body.p.x, @body.p.y, 1, @body.a.radians_to_gosu)
-    
-    # draw ghost
-    if @boundary[:left_edge]
-      @image.draw_rot(@body.p.x + @scene.width, @body.p.y, 1, @body.a.radians_to_gosu)
-    end
-    if @boundary[:right_edge]
-      @image.draw_rot(@body.p.x - @scene.width, @body.p.y, 1, @body.a.radians_to_gosu)
-    end
-    if @boundary[:top_edge]
-      @image.draw_rot(@body.p.x, @body.p.y + @scene.height, 1, @body.a.radians_to_gosu)
-    end
-    if @boundary[:bottom_edge]
-      @image.draw_rot(@body.p.x, @body.p.y - @scene.height, 1, @body.a.radians_to_gosu)
-    end
-    if @boundary[:top_edge] && @boundary[:left_edge]
-      @image.draw_rot(@body.p.x + @scene.width, @body.p.y + @scene.height, 1, @body.a.radians_to_gosu)
-    end
-    if @boundary[:top_edge] && @boundary[:right_edge]
-      @image.draw_rot(@body.p.x - @scene.width, @body.p.y + @scene.height, 1, @body.a.radians_to_gosu)
-    end
-    if @boundary[:bottom_edge] && @boundary[:right_edge]
-      @image.draw_rot(@body.p.x - @scene.width, @body.p.y - @scene.height, 1, @body.a.radians_to_gosu)
-    end
-    if @boundary[:bottom_edge] && @boundary[:left_edge]
-      @image.draw_rot(@body.p.x + @scene.width, @body.p.y - @scene.height, 1, @body.a.radians_to_gosu)
-    end
+    self.draw_with_boundary
   end
 
 end
