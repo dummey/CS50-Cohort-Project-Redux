2a6ebbe337907fca6587212bf9be7edbb1b682ac
jbtule <jay+code@tuley.name>
2015-08-26 21:39:43 -0500

    change defaults to make game playable

--- lib/game_objects/asteroid.rb
+++ lib/game_objects/asteroid.rb
@@ -22,9 +22,9 @@ class Asteroid < GameObject
       :z_index => 1,
       :init_rotate => 0,
       :tier => 1,
-      :max_tier => 4,
+      :max_tier => 3,
       :collision_type => :asteroid,
-      :collision_sensor => false,
+      :collision_sensor => true,
       :bit_plane => 0b11,
     }
   end
