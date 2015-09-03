cc0d30bab21f6545c295a9bd103fdbe4c511db3d
Ther <emeraldvision@users.noreply.github.com>
2015-09-02 20:16:27 -0500

    refactor subtitle

--- lib/game_objects/ui_components/subtitle.rb
+++ lib/game_objects/ui_components/subtitle.rb
@@ -13,7 +13,7 @@ class Subtitle < GameObject
       :x_pos => @scene.width/2,
       :y_pos => @scene.height/2 + 50,
       :z_index => 1,
-      :scale => 1,
+      :scale => 0.75,
       :angle => 0,
     }.merge(params)
   end
@@ -36,7 +36,7 @@ class Subtitle < GameObject
     @text_image.draw_rot(@x_pos + 2, @y_pos + 2, @z_index, 0.5, 0.5, 0x40_000000)
     @text_image.draw_rot(@x_pos, @y_pos, @z_index, 0.5, 0.5)
 
-    @spaceship_image.draw_rot(@x_pos, @y_pos - 30, @z_index, @angle, 0.5, 0.5, 0.75, 0.75)
+    @spaceship_image.draw_rot(@x_pos, @y_pos - 30, @z_index, @angle, 0.5, 0.5, scale, scale)
 
     self
   end
